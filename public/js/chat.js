8fc4bacbb2f62eb2e160e17320750f2333515c3f
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.home>
2019-09-07 22:56:18 -0400

    aknowledges the changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,28 @@
 const socket = io()
+
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocation = document.querySelector("#send-location")
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector("#message-form").addEventListener("submit", (e) =>{
+$messageForm.addEventListener("submit", (e) =>{
     e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
    const message = e.target.elements.message.value
 
    socket.emit('sendMessage', message, (error) => {
+
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
        if(error){
            return console.log(error)
        }
@@ -16,16 +30,17 @@ document.querySelector("#message-form").addEventListener("submit", (e) =>{
    })
 })
 
-document.querySelector("#send-location").addEventListener("click", () =>{
+$sendLocation.addEventListener("click", () =>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
     }
-
+    $sendLocation.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude : position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
+            $sendLocation.removeAttribute('disabled')
             console.log('Location is shared!')
         })
     })
